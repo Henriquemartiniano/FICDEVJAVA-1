8dc7eb31dccb0efde46ea61520ed3de2a494fbcd
Henrique Martiniano <131673948+Henriquemartiniano@users.noreply.github.com>
2023-06-15 10:30:14 -0400

    Submissão do dia 12

---
+++ Capitulo12/src/main/java/com/mycompany/capitulo12/Capitulo12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capitulo12;
+
+/**
+ *
+ * @author ficdev
+ */
+public class Capitulo12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
