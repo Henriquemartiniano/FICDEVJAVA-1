c05a602932cf41238ea80e92be3d424188aa5e89
Henrique Martiniano <131673948+Henriquemartiniano@users.noreply.github.com>
2023-06-22 10:44:26 -0400

    Submissão do dia 17

--- Capitulo15/src/main/java/com/mycompany/capitulo15/TarefasApi.java
+++ Capitulo15/src/main/java/com/mycompany/capitulo15/TarefasApi.java
@@ -3,41 +3,38 @@ package com.mycompany.capitulo15;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
+
 @SpringBootApplication
 @RestController
-@RequestMapping ("/api/tarefas")
+@RequestMapping("/api/tarefas")
 public class TarefasApi {
-private List < String > tarefas = new ArrayList < >() ;
-@GetMapping
-public List < String > listarTodasTarefas () {
-return  getTarefas() ;
-}
-@PostMapping
-public void criarTarefa ( @RequestBody String tarefa ) {
-        getTarefas() . add ( tarefa ) ;
-}
-@DeleteMapping ("/{index}")
-public void apagarTarefa ( @PathVariable int id ) { if ( id >= 0 && id < getTarefas() . size () ) {
-            getTarefas() . remove ( id ) ;
-}
-}
-public static void main ( String [] args ) {
-SpringApplication . run ( TarefasApi .class , args ) ;
-}
 
-    /**
-     * @return the tarefas
-     */
-    public List < String > getTarefas() {
+
+
+    private final List< String> tarefas = new ArrayList<>();
+
+    @GetMapping
+    public List< String> listarTodasTarefas() {
         return tarefas;
     }
 
-    /**
-     * @param tarefas the tarefas to set
-     */
-    public void setTarefas(List < String > tarefas) {
-        this.tarefas = tarefas;
+    @PostMapping
+    public void criarTarefa(@RequestBody String tarefa) {
+        tarefas.add(tarefa);
     }
+
+    @DeleteMapping("/{id}")
+    public void apagarTarefa(@PathVariable int id) {
+        if (id >= 0 && id < tarefas.size()) {
+            tarefas.remove(id);
+        }
+    }
+    
+    public static void main ( String[] args ) {
+    SpringApplication.run(TarefasApi.class , args ) ;
+    }
+    
 }
